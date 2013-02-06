3820fb1e5561d6c22b6a5b5a0cbdcf771070d27b
Michael Grubb <devel@dailyvoid.com>
2013-02-06 17:32:42 -0600

    clojure tooling configs

---
+++ lein/profiles.clj
@@ -0,0 +1,9 @@
+{:user
+  {:plugins [[lein-localrepo "0.4.1"]
+             [lein-clean-m2 "0.1.2"]
+             [lein-gitify "0.1.0"]
+             [lein-outdated "1.0.0"]
+             [lein-midje "2.0.3"]
+             [lein-kibit "[0.0.7,)"]]
+   :dependencies [[com.stuartsierra/lazytest "1.2.3"]]
+   :repositories [["stuart" "http://stuartsierra.com/maven2"]]}}

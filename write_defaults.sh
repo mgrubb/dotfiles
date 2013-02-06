e5056695e0622537d63dca07ed3b6af967c5181e
Michael Grubb <devel@dailyvoid.com>
2013-02-06 17:36:06 -0600

    misc vim config changes

--- write_defaults.sh
+++ write_defaults.sh
@@ -1,3 +1,4 @@
 #!/bin/ksh
 # Set the Time Machine backup interval to every 4 hours
 sudo defaults write /System/Library/LaunchDaemons/com.apple.backupd-auto.plist StartInterval -int 14400
+sudo defaults write /System/Library/Preferences/com.apple.LaunchServices.plist LSQuarantine -bool NO

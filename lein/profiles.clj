9f31c33b690c7de783d79d75723a79b7946dc30b
Michael Grubb <devel@dailyvoid.com>
2013-05-16 11:46:08 -0500

    added conjure plugin for lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,7 +4,8 @@
              [lein-gitify "0.1.0"]
              [lein-outdated "1.0.0"]
              [lein-midje "2.0.3"]
-             [lein-kibit "[0.0.7,)"]]
+             [lein-kibit "[0.0.7,)"]
+             [org.conjure/lein-conjure "1.0.0"]]
    :injections [(defn classpath [] (seq (-> (ClassLoader/getSystemClassLoader) (.getURLs))))
                 (defn show-classpath [] (doseq [url (classpath)] (println (.getFile url))))]
    :dependencies [[com.stuartsierra/lazytest "1.2.3"]

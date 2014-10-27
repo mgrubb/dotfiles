95e70e0d911e59757ebcc5128fa63c29eab70440
Michael Grubb <michael@grubb.co>
2014-10-26 21:51:50 -0500

    add plugins to lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,13 +1,22 @@
 {:user
-  {:plugins [[lein-localrepo "0.4.1"]
-             [lein-clean-m2 "0.1.2"]
-             [lein-gitify "0.1.0"]
-             [lein-outdated "1.0.0"]
-             [lein-midje "2.0.3"]
-             [lein-kibit "[0.0.7,)"]
-             [org.conjure/lein-conjure "1.0.0"]]
-   :injections [(defn classpath [] (seq (-> (ClassLoader/getSystemClassLoader) (.getURLs))))
+  {:plugins [[lein-localrepo "LATEST"]
+             [lein-clean-m2 "LATEST"]
+             [lein-gitify "LATEST"]
+             [lein-outdated "LATEST"]
+             [lein-midje "LATEST"]
+             [lein-kibit "LATEST"]
+             [lein-difftest "LATEST"]
+             [lein-try "LATEST"]
+             [lein-marginalia "LATEST"]
+             [slamhound "LATEST"]
+             [org.conjure/lein-conjure "LATEST"]]
+   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+   :signing {:gpg-key "8D0BFAB7"}
+   :injections [(require '[poker.refactor])
+                (defn classpath [] (seq (-> (ClassLoader/getSystemClassLoader) (.getURLs))))
                 (defn show-classpath [] (doseq [url (classpath)] (println (.getFile url))))]
    :dependencies [; [com.stuartsierra/lazytest "1.2.3"]
-                  [clj-stacktrace "0.2.5"]]
+                  [clj-stacktrace "LATEST"]
+                  [poker "0.1.0-SNAPSHOT"]
+                  ];[skel/lein-template "1.0.0"]]
    }}

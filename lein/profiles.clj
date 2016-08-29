1befeb1ad0c61380de185e23613f9a48358007b8
Michael Grubb <michael@grubb.co>
2016-08-29 13:27:21 -0500

    cleanup of leiningen profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,15 @@
-{:user
+{
+;; :repl {:plugins [[cider/cider-nrepl "LATEST"]]
+;;        :repl-options {:nrepl-middleware
+;;                  [cider.nrepl.middleware.pprint/wrap-pprint
+;;                   cider.nrepl.middleware.format/wrap-format]}}
+ :user
   {:plugins [[lein-localrepo "LATEST"]
              [lein-pprint "LATEST"]
              [lein-clean-m2 "LATEST"]
              [lein-gitify "LATEST"]
-             [lein-outdated "LATEST"]
-             [lein-midje "3.1.5-SNAPSHOT"]
+             [lein-ancient "LATEST"]
+             [lein-midje "LATEST"]
              [lein-kibit "LATEST"]
              [lein-difftest "LATEST"]
              [lein-try "LATEST"]
@@ -17,21 +22,18 @@
              [mvxcvi/whidbey "LATEST"]
              [lein-fore-prob "LATEST"]
              [gargamel "LATEST"]
-             [cider/cider-nrepl "LATEST"]
              [lein-cljfmt "LATEST"]]
-   :puget-options {:width 132}
+   :whidbey {:width 132}
    :aliases {"slamhound" ["run" "-m" "slam.hound"]}
    :signing {:gpg-key "8D0BFAB7"}
    :deploy-repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]
                          ["snapshots" {:url "s3p://yggdrasil-private/snapshots/" :creds :gpg}]
                          ["private" {:url "s3p://yggdrasil-private/releases/" :creds :gpg}]]
-   :injections [(require '[poker.refactor])
-                (require 'spyscope.core)
+   :injections [(require 'spyscope.core)
                 (defn classpath [] (seq (-> (ClassLoader/getSystemClassLoader) (.getURLs))))
                 (defn show-classpath [] (doseq [url (classpath)] (println (.getFile url))))]
    :dependencies [; [com.stuartsierra/lazytest "1.2.3"]
                   [clj-stacktrace "0.2.8"]
-                  [poker "0.1.0-SNAPSHOT"]
                   [spyscope "0.1.5"]]
                 ; [skel/lein-template "1.0.0"]]
    }}

0897e3757deb276df72c2f47e4ab1aeb956152a5
Michael Grubb <michael@grubb.co>
2016-09-29 22:41:19 -0500

    fix bug with keybinding

--- .emacs.d/lisp/general-init.el
+++ .emacs.d/lisp/general-init.el
@@ -1,32 +1,34 @@
 (use-package general
   :ensure t
-  :commands (evil-mode)
+  :demand t
   :config
-  (general-define-key :prefix nil
-                      :states '(normal)
-                      "j" 'evil-next-visual-line
-                      "k" 'evil-previous-visual-line)
+  (progn
+    (general-define-key :prefix nil
+                        :states '(normal)
+                        "j" 'evil-next-visual-line
+                        "k" 'evil-previous-visual-line)
 
-  (general-define-key :states '(normal)
-                      "xk" 'kill-buffer)
+    (general-define-key :states '(normal)
+                        "xk" 'kill-buffer)
 
-  (general-define-key :prefix "g"
-                      "j" 'evil-next-line
-                      "k" 'evil-previous-line)
+    (general-define-key :prefix "g"
+                        :states '(normal)
+                        "j" 'evil-next-line
+                        "k" 'evil-previous-line)
 
-  (general-define-key :keymaps 'paredit-mode-map
-                      :states '(emacs normal)
-                      "W" 'paredit-wrap-round
-                      "(" 'paredit-wrap-round
-                      "w[" 'paredit-wrap-square
-                      "w{" 'paredit-wrap-curly
-                      "w\"" 'paredit-meta-doublequote
-                      "s" 'paredit-splice-sexp
-                      ">" 'paredit-forward-slurp-sexp
-                      "<" 'paredit-backward-slurp-sexp
-                      "j" 'paredit-join-sexps)
+    (general-define-key :keymaps 'paredit-mode-map
+                        :states '(normal)
+                        "W" 'paredit-wrap-round
+                        "(" 'paredit-wrap-round
+                        "w[" 'paredit-wrap-square
+                        "w{" 'paredit-wrap-curly
+                        "w\"" 'paredit-meta-doublequote
+                        "s" 'paredit-splice-sexp
+                        ">" 'paredit-forward-slurp-sexp
+                        "<" 'paredit-backward-slurp-sexp
+                        "j" 'paredit-join-sexps))
   :init
-  (setq general-default-prefix ",")
-
-  (setq general-default-keymaps '(evil-normal-state-map)))
+  (progn
+    (setq general-default-prefix ",")
+    (setq general-default-keymaps '(evil-normal-state-map global))))
 

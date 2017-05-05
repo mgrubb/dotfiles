bb0d8fef33156c253110eec7ab1063f35164604c
Michael Grubb <michael@grubb.co>
2017-05-05 15:27:05 -0500

    added capture templates

--- .emacs.d/lisp/org-init.el
+++ .emacs.d/lisp/org-init.el
@@ -2,9 +2,16 @@
   :pin org
   :ensure t
   :mode ("\\.org\\'" . org-mode)
-  :init
+  :config
+  
   (setq org-directory "~/Documents/Notes")
   (setq org-default-notes-file (concat org-directory "/notes.org"))
+  (setq org-capture-templates
+        '(("t" "Technical Notes")
+          ("tg" "General" entry (file (concat org-directory "/tn-general.org"))
+           "* %?\n    %i\n%a\n%t")
+          ("tu" "Unix" entry (file (concat org-directory "/tn-unix.org"))
+           "* %? %^g\n    %i")))
   (setq org-tag-alist '(("worklog" . ?l)))
   (setq org-agenda-files '("~/Documents/Notes"))
   (setq default-major-mode 'org-mode))

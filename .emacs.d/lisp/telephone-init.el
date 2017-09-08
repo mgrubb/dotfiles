d360462f64c8e24de5491a7366a66da20dc4b02e
Michael Grubb <michael@grubb.co>
2017-09-08 14:20:13 -0500

    fixed evil/telphone config after tele update

--- .emacs.d/lisp/telephone-init.el
+++ .emacs.d/lisp/telephone-init.el
@@ -4,21 +4,6 @@
   :init
   (setq telephone-line-evil-use-short-tag 1)
   :config
-  (setq telephone-line-lhs
-	'((evil . (telephone-line-evil-tag-segment))
-	  (accent . (telephone-line-vc-segment
-		     telephone-line-erc-modified-channels-segment
-		     telephone-line-process-segment))
-	  (nil . (telephone-line-minor-mode-segment
-		  telephone-line-buffer-segment))))
-
-  (setq telephone-line-rhs
-	'((nil . (telephone-line-misc-info-segment))
-	  (accent . (telephone-line-major-mode-segment))
-	  (evil . (telephone-line-airline-position-segment))))
-
   (telephone-line-mode 1))
 
-;; (use-package powerline
-;;   :ensure t)
 (provide 'telephone-init)

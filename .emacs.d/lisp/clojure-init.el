6ca240784bac6dec8547295155ff44647e830350
Michael Grubb <michael@grubb.co>
2017-09-09 19:41:25 -0500

    local changes

--- .emacs.d/lisp/clojure-init.el
+++ .emacs.d/lisp/clojure-init.el
@@ -1,13 +1,16 @@
 (use-package clojure-mode
   :ensure t
   :commands (clojure-mode)
-  :mode (("\\.clj[scx]?\\'?" . clojure-mode))
+  :mode (("\\.clj[cx]?\\'" . clojure-mode)
+         ("\\.cljs\\'" . clojurescript-mode))
   :init
   (setq clojure-align-forms-automatically 1)
   (add-hook 'clojure-mode-hook #'subword-mode)
   (add-hook 'clojure-mode-hook #'paredit-mode)
   (add-hook 'clojure-mode-hook #'evil-paredit-mode)
   :config
+  (define-clojure-indent
+    (defui '(1 :form :form (1))))
   (use-package cider
     :ensure t
     :commands (cider-mode cider-jack-in)
--- .emacs.d/lisp/telephone-init.el
+++ .emacs.d/lisp/telephone-init.el
@@ -3,21 +3,13 @@
   :demand t
   :init
   (setq telephone-line-evil-use-short-tag 1)
-  :config
-  (setq telephone-line-lhs
-	'((evil . (telephone-line-evil-tag-segment))
-	  (accent . (telephone-line-vc-segment
-		     telephone-line-erc-modified-channels-segment
-		     telephone-line-process-segment))
-	  (nil . (telephone-line-minor-mode-segment
-		  telephone-line-buffer-segment))))
-
   (setq telephone-line-rhs
-	'((nil . (telephone-line-misc-info-segment))
-	  (accent . (telephone-line-major-mode-segment))
-	  (evil . (telephone-line-airline-position-segment))))
-
-  (telephone-line-mode 1))
+        '((nil . (telephone-line-misc-info-segment))
+          (accent . (telephone-line-major-mode-segment))
+          (evil . (telephone-line-airline-position-segment))))
+  :config
+  (require 'telephone-line-config)
+  (telephone-line-evil-config))
 
 ;; (use-package powerline
 ;;   :ensure t)

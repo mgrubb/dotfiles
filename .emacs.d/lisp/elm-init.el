ac14fd96cad05956491096e24ad6944bb732cc7f
Michael Grubb <Michael.Grubb@acxiom.com>
2021-03-19 10:50:00 -0500

    Prepare for Chezmoi Merge

--- .emacs.d/lisp/elm-init.el
+++
@@ -1,6 +0,0 @@
-(use-package elm-mode
-  :ensure t
-  :init
-  (add-hook 'elm-mode-hook (lambda () (aggressive-indent-mode -1))))
-
-(provide 'elm-init)
--- .emacs.d/lisp/help-init.el
+++
@@ -1,5 +0,0 @@
-(use-package help+ :ensure t)
-(use-package help-fns+ :ensure t)
-(use-package help-mode+ :ensure t)
-
-(provide 'help-init)
